3ca13f370fcd973e40b98d21fc85ade7a41bdf8b
Henrique Breda <henrique.breda@feedzai.com>
2022-08-11 12:52:35 +0100

    ini repo

---
+++ vars/pipeline-java.groovy
@@ -0,0 +1,34 @@
+def pipelineJava() {
+  pipeline {
+    agent any
+    environment {
+        BUZZ = 'FIZZ'
+    }
+    stages {
+        stage('Build') {
+          stage('Compile') {
+            steps {
+              echo 'Compile..'
+            }
+          }
+        }
+        parallel {
+          stage('validate') {
+            steps {
+              echo 'Validate..'
+            }
+          }
+          stage('Test') {
+            steps {
+              echo 'Testing..'
+            }
+          }
+        }
+        stage('Deploy') {
+          steps {
+            echo 'Deploying....'
+          }
+        }
+    }
+  }
+}
