11e9166ffdf533d856e540ee835a35969d40f932
Henrique Breda <henrique.breda@feedzai.com>
2022-08-11 15:50:23 +0100

    feat added test step

--- vars/pipelineJava.groovy
+++ vars/pipelineJava.groovy
@@ -13,6 +13,40 @@ def call() {
                 echo 'Build..'
             }
         }
+        stage('Test') {
+            parallel {
+                stage('Unit Test') {
+                    steps {
+                        echo 'Unit Tests..'
+                    }
+                    post {
+                        always {
+                            echo 'After Unit Tests..'
+                        }
+                    }
+                }
+                stage('Component Tests') {
+                    steps {
+                        echo 'Component Tests..'
+                    }
+                    post {
+                        always {
+                            echo 'After Component Tests..'
+                        }
+                    }
+                }
+                stage('Integration Tests') {
+                    steps {
+                        echo 'Integration Tests..'
+                    }
+                    post {
+                        always {
+                            echo 'After Integration Tests..'
+                        }
+                    }
+                }
+            }
+        }
         stage('Code Metrics') {
             steps {
                 echo 'Code Metrics..'
